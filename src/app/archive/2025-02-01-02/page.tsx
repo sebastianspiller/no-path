fd9c3a117bf1edbeae73d06679addf554180b63e
Sebastian Spiller <sebastian.spiller@trusted.care>
2025-02-02 09:41:29 +0100

    is it an idea, or is it the process?

---
+++ src/app/archive/2025-02-01-02/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+const AnimationLottie = dynamic(() => import("../../AnimationLottie"), {
+  ssr: false,
+});
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <AnimationLottie />
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <p className="text-sm underline text-gray-500">
+          <Link href="/archive/2025-02-01-01">Do not follow footsteps.</Link>
+        </p>
+      </footer>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,26 @@ const AnimationLottie = dynamic(() => import("./AnimationLottie"), {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid items-center justify-items-center min-h-screen pt-1 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-1 justify-center">
+        <AnimationLottie />
         <AnimationLottie />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="flex flex-col gap-2 justify-center items-center lg:items-start">
+        <p className="text-sm text-gray-500">
+          The way you see, it looks different.
+        </p>
+        <p className="text-sm text-gray-500">
+          Will there be connection, where interpretation is?
+        </p>
+        <p className="text-sm text-gray-500">
+          The more there is, the less there is.
+        </p>
+        <p className="text-sm text-gray-500">Too much information</p>
+        <p className="text-sm text-gray-500">No words, just form.</p>
+        <p className="mb-16 text-sm text-gray-500">tl;dr</p>
         <p className="text-sm underline text-gray-500">
-          <Link href="/archive/2025-02-01">Do not follow footsteps.</Link>
+          <Link href="/archive/2025-02-01-02">A link to no past.</Link>
         </p>
       </footer>
     </div>
